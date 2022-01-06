b672cfbf7e8502a5bff582c8d250dc390744ab29
Joe Carter <joe_e_carter@hotmail.co.uk>
2022-01-06 21:57:07 +0000

    Removed some unused files from the boilerplate

---
+++ packages/dice-roller/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './global.css'
+import DiceRollerApp from './DiceRollerApp/DiceRollerApp'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DiceRollerApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
