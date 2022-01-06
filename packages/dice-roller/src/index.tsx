3d9e299d16bc4bbe92f2ec9d6caa8337e0f284f1
Joe Carter <joe_e_carter@hotmail.co.uk>
2022-01-06 22:39:01 +0000

    Added missing lisense and installed node-sass

--- packages/dice-roller/src/index.tsx
+++ packages/dice-roller/src/index.tsx
@@ -1,6 +1,6 @@
+import './global.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './global.css'
 import DiceRollerApp from './DiceRollerApp/DiceRollerApp'
 
 ReactDOM.render(
