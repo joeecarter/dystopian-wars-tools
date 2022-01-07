9a6c1b62c8447b317ff5a82562fde36c64e6dc4b
Joe Carter <joe_e_carter@hotmail.co.uk>
2022-01-07 00:20:11 +0000

    Start of the hold space to roll system

--- packages/dice-roller/src/DiceRollerApp/DiceRollerApp.tsx
+++ packages/dice-roller/src/DiceRollerApp/DiceRollerApp.tsx
@@ -1,15 +1,88 @@
 import './DiceRollerApp.scss'
 import Dice from '../Dice/Dice'
+import { useEffect, useRef, KeyboardEvent, LegacyRef } from 'react'
+import { useState } from 'react'
 
 export default function DiceRollerApp() {
+
+	const [ dice, setDice ] = useState<Array<number>>([])
+	const [ diceCount, setDiceCount ] = useState<number>(8)
+
+	const [ rolling, setRolling ] = useState<boolean>(false)
+
+	const div = useRef<HTMLDivElement>(null)
+	const timer = useRef<NodeJS.Timeout>();
+
+	useEffect(() => {
+		setDice(createEmptyRoll(diceCount))
+
+		// startRoller()
+		if (div.current) {
+			(div.current as unknown as HTMLDivElement).focus()
+		}
+		return () => { stopRoller() }
+	}, [])
+
+
+	const startRoller = () => {
+		if (rolling) {
+			return false
+		}
+
+		timer.current = setInterval(() => {
+			setDice(createRoll(diceCount))
+		}, 100)
+
+		setRolling(true)
+
+		return true
+	}
+
+	const stopRoller = () => {
+		if (!rolling) {
+			return false
+		}
+
+		if (timer.current) {
+			clearInterval(timer.current)
+		}
+
+		setRolling(false)
+
+		return true
+	}
+
+	const onKeyUp = (event: KeyboardEvent<HTMLDivElement>): void => {
+		if (event.code === 'Space') stopRoller()
+	}
+
+	const onKeyDown = (event: KeyboardEvent<HTMLDivElement>): void => {
+		if (event.code === 'Space') startRoller()
+	}
+
 	return (
-	<div className="DiceRollerApp">
-		<Dice value={1} />
-		<Dice value={2} />
-		<Dice value={3} />
-		<Dice value={4} />
-		<Dice value={5} />
-		<Dice value={6} />
-	</div>
+		<div
+			ref={div}
+			tabIndex={-1}
+			className="DiceRollerApp"
+			onKeyUp={onKeyUp}
+			onKeyDown={onKeyDown}
+		>
+			{dice.map((dice, index) => <Dice key={index} value={dice} />)}
+		</div>
 	)
+}
+
+const randomDiceRoll = () => Math.floor(Math.random() * 6) + 1
+
+function createRoll(diceCount: number) {
+	let rolls = []
+	for (let i = 0; i < diceCount; i++) rolls.push(randomDiceRoll())
+	return rolls
+}
+
+function createEmptyRoll(diceCount: number) {
+	let rolls = []
+	for (let i = 0; i < diceCount; i++) rolls.push(diceCount)
+	return rolls
 }
--- packages/dice-roller/src/index.tsx
+++ packages/dice-roller/src/index.tsx
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom'
 import DiceRollerApp from './DiceRollerApp/DiceRollerApp'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DiceRollerApp />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<DiceRollerApp />
+	</React.StrictMode>,
+	document.getElementById('root')
 )
